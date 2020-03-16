22893ff4bff8b8f3dd516e54cd7906024e4f78f1
Ubuntu <ubuntu@ip-172-30-2-123.us-east-2.compute.internal>
2020-03-16 14:38:01 +0000

    initial commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      name
+      owner
+      date
+      description
+      completed
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        owner
+        date
+        description
+        completed
+      }
+      nextToken
+    }
+  }
+`;
+export const searchTodos = /* GraphQL */ `
+  query SearchTodos(
+    $filter: SearchableTodoFilterInput
+    $sort: SearchableTodoSortInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    searchTodos(
+      filter: $filter
+      sort: $sort
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        name
+        owner
+        date
+        description
+        completed
+      }
+      nextToken
+      total
+    }
+  }
+`;
